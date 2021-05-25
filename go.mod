a3f82a317465af00deb44d5d8d00f946ad1ae45f
Lapi <tyoutugai.7@gmail.com>
2021-05-25 20:41:58 +0900

    add go-chi/jwtauth

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/dragoneena12/lapi-hotel-system
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0 // indirect
-	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/jwtauth/v5 v5.0.1
+	github.com/goccy/go-json v0.5.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/jwx v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/vektah/gqlparser/v2 v2.2.0
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 )
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -9,9 +10,22 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/dragoneena12/lapi-hotel-system/graph"
 	"github.com/dragoneena12/lapi-hotel-system/graph/generated"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 const defaultPort = "8080"
+
+var tokenAuth *jwtauth.JWTAuth
+
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
+
+	// For debugging/example purposes, we generate and print
+	// a sample jwt token with claims `user_id:123` here:
+	_, tokenString, _ := tokenAuth.Encode(map[string]interface{}{"user_id": 123})
+	fmt.Printf("DEBUG: a sample jwt is %s\n\n", tokenString)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -19,11 +33,14 @@ func main() {
 		port = defaultPort
 	}
 
+	router := chi.NewRouter()
+	router.Use(jwtauth.Verifier(tokenAuth))
+
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
